12c30cc0b408aa337a2744ee0e080c838be5b26c
Tony Yip <tony@opensource.hk>
2017-01-09 01:29:37 +0800

    Add PostCSS processor

--- gulp/build-css.js
+++ gulp/build-css.js
@@ -6,21 +6,15 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const cleanCss = require('gulp-clean-css');
-const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
-
-const processor = [
-  autoprefixer({
-    browsers: ['last 5 version']
-  })
-];
+const plugins = require('./postcss');
 
 gulp.task('build:css', 'Build ./assets/scss/*.scss production version into ./public', () => {
   return gulp.src('./assets/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .on('error', util.log)
-    .pipe(postcss(processor))
+    .pipe(postcss(plugins))
     .pipe(cleanCss({level: 2}))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public'));
--- gulp/dev-css.js
+++ gulp/dev-css.js
@@ -5,21 +5,15 @@ const util = require('gulp-util');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
-const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
-
-const processor = [
-  autoprefixer({
-    browsers: ['last 5 version']
-  })
-];
+const plugins = require('./postcss');
 
 gulp.task('dev:css', 'Build development version ./assets/scss/*.scss into ./public', () => {
   return gulp.src('./assets/scss/*.scss')
     .on('error', util.log)
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(postcss(processor))
+    .pipe(postcss(plugins))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public'));
 });
