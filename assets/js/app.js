438c3eaaac657d03c7ea2d265523f93789f9eea9
Koala Yeung <koalay@gmail.com>
2017-03-23 21:17:52 +0800

    Rewrite links for cross domain tracking
    Reference:
    1. https://www.eventbrite.com/support/articles/en_US/Troubleshooting/how-to-enable-cross-domain-and-ecommerce-tracking-with-google-universal-analytics?lg=en_US
    2. https://developers.google.com/analytics/devguides/collection/analyticsjs/cross-domain

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,23 @@ require('./component/collapsible');
 $('[data-activates]').sideNav();
 $('.collapsible').collapsible();
 
+(function () {
+
+  // rewrite links to add cross domain tracking
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga(function(tracker) {
+    var clientId = tracker.get('clientId');
+    var clientID = 'hello';
+    $('a').each(function () {
+      var link = this;
+      if (link.hostname.match(/^(.+?\.|)eventbrite\.com/)) {
+        link.search = link.search + '_eboga=' + clientID;
+      }
+    });
+  });
+
+})();
+
 require.ensure(['node-waves'], () => {
   const Waves = require('node-waves');
   Waves.init();
