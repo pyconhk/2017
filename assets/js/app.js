9cc100d68f002af6330e839839f1bee5910a6239
Koala Yeung <koalay@gmail.com>
2017-03-23 22:16:30 +0800

    Add outbound link event tracking

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,15 +10,46 @@ $('.collapsible').collapsible();
 
 (function () {
 
-  // rewrite links to add cross domain tracking
+  // ensure GA is here
   window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+
+  // to track normal outbound links
+  const trackTicketOutbound = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {
+      'transport': 'beacon',
+      'hitCallback': function(){document.location = url;}
+    });
+  }
+
+  // to track Eventbrite links
+  const trackEventbriteOutbound = function(category, action, label, value){
+    return function () {
+      ga('send', 'event', category, action, label, value);
+    }
+  }
+
   ga(function(tracker) {
-    var clientId = tracker.get('clientId');
-    var clientID = 'hello';
+
+    // get ga client ID
+    const clientID = tracker.get('clientId');
+
     $('a').each(function () {
-      var link = this;
+      let link = this;
+      let $link = $(this);
       if (link.hostname.match(/^(.+?\.|)eventbrite\.com/)) {
+
+        // rewrite links to add cross domain tracking
         link.search = link.search + '_eboga=' + clientID;
+
+        // track outbound link
+        const category = $link.data('category');
+        const action   = $link.data('action');
+        const label    = $link.data('label');
+        const value    = $link.data('value');
+        if ((typeof category != 'undefined') & (category != '')
+          && (typeof action != 'undefined') && (action != '')) {
+          this.onClick = trackEventbriteOutbound(category, action, label, value);
+        }
       }
     });
   });
