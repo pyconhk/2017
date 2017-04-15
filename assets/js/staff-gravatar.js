4fdeacb0230c9114ebbdaab83176642a07039a2f
Connie <cleung334@gmail.com>
2017-04-15 18:47:46 +0800

    Use JQuery to set the src attribute of image

--- assets/js/staff-gravatar.js
+++ assets/js/staff-gravatar.js
@@ -2,7 +2,7 @@
 
 import gravatar from 'gravatar';
 
-export function getImage(email) {
+export function getImageUrl(email) {
 
   const option = {
     s: '100',
@@ -11,3 +11,14 @@ export function getImage(email) {
   };
   return gravatar.url (email, option);
 }
+
+$(document).ready(function() {
+  $("li[data-email]").each(function(i) {
+    let $this = $(this);
+    let url = getImageUrl($this.data('email'));
+    let imgChild = $this.find('img');
+    if (imgChild) {
+      imgChild.prop('src', url);
+    }
+  });
+});
