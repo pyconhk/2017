12ceb34c5ca585016fd6138367f38d51ffaaa0d6
Koala Yeung <koalay@gmail.com>
2017-05-11 11:16:01 +0800

    Basic for topic pages generation

--- gulp/build-html.js
+++ gulp/build-html.js
@@ -7,6 +7,17 @@ const util = require('gulp-util');
 const path = require('path');
 const htmldata = require('./includes/html-data');
 const htmlurl = require('./includes/html-url-append');
+const requireyml = require('require-yml');
+
+// base path
+const basepath = path.dirname(__dirname);
+
+function swallowTopicRenderError(topic) {
+  return function (error) {
+    util.log(`Failed on '${util.colors.cyan('dev:html')}' ('${topic.id}'): ${error.toString()}`);
+    this.emit('end')
+  }
+}
 
 gulp.task('build:html', 'Build ./assets/pages/*.jinja into production HTML files', () => {
   const {Environment, FileSystemLoader} = require('nunjucks');
@@ -16,8 +27,11 @@ gulp.task('build:html', 'Build ./assets/pages/*.jinja into production HTML files
   ]);
   htmlurl.addFilters(env);
 
-  return gulp.src('assets/pages/**/*.jinja')
-    .pipe(data(htmldata.fileData))
+  gulp.src([
+    'assets/pages/**/*.jinja',
+    '!assets/pages/**/_*.jinja',
+  ])
+    .pipe(data(htmldata.fileData()))
     .pipe(require('gulp-nunjucks').compile({}, {env}))
     .pipe(require('gulp-htmlmin')({
       collapseWhitespace: true
@@ -27,4 +41,27 @@ gulp.task('build:html', 'Build ./assets/pages/*.jinja into production HTML files
     }))
     .on('error', util.log)
     .pipe(gulp.dest('public'));
+
+  let assetData = requireyml(basepath + '/assets/data');
+
+  for (let topic of assetData.topics.topics) {
+    util.log(`Generate: '/topics/${util.colors.magenta(topic.id)}/index.html'`);
+    var pageID = 'page--topics--' + topic.id;
+    gulp.src(`assets/pages/topics/_topic.jinja`)
+      .pipe(data(htmldata.fileData({
+        topic,
+        pageID,
+      })))
+      .pipe(require('gulp-nunjucks').compile({}, {
+        env,
+      }).on('error', swallowTopicRenderError(topic)))
+      .pipe(require('gulp-rename')({
+        dirname: topic.id,
+        basename: 'index',
+        extname: '.html',
+      }))
+      .on('error', util.log)
+      .pipe(gulp.dest('public/topics'));
+  }
+
 });
