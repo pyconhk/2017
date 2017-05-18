1d60a8c1e2a64e93a3efb64c3afa0f1f0f5de6d1
Tony Yip <tony@opensource.hk>
2017-05-19 03:04:07 +0800

    Fix button cover title and unity of timetable element

--- assets/js/timetable/container/SignInModal.js
+++ assets/js/timetable/container/SignInModal.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import SignInModal from '../component/SignInModal';
 import type { State } from '../../reducers';
 
-function mapStateToProps(state: State) {
+function mapStateToProps(state: State, ownProps: {position: 'navbar' | 'sidebar'}) {
   return {
     user: state.user,
+    ...ownProps,
   };
 }
 
