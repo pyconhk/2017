ee02e1cb7b53f750b4ed5a70f4f350a5af1cf4a5
Tony Yip <tony@opensource.hk>
2017-05-20 01:37:50 +0800

    Show session part

--- assets/js/timetable/component/TopicCell.jsx
+++ assets/js/timetable/component/TopicCell.jsx
@@ -8,6 +8,7 @@ import Speakers from '../container/Speakers';
 /* eslint-disable */
 type Props = {
   col: number,
+  name: string,
   topic: {
     title: string,
     id: string,
@@ -20,10 +21,12 @@ type Props = {
 export default function TopicCell(props: Props) {
   const topic = props.topic;
   const link = `/2017/topics/${topic.id}/`;
+  const part = props.name.match(/Part\s(\d+)/im);
+  const title = Array.isArray(part) ? `${topic.title} Part ${part[1]}` : topic.title;
   return (
     <GridCell col={props.col} className="topic-grid grid" href={link} target="_blank">
       <div className="session-title">
-        {topic.title}
+        {title}
       </div>
       <div className="session-details">
         <ul>
