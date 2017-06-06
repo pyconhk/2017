bf514c14bf8fde8b5a95003c5b1f979e95ffc4ba
Koala Yeung <koalay@gmail.com>
2017-06-06 15:29:35 +0800

    Further improve look and feel for topic modal

--- assets/js/timetable/component/ModalProvider.jsx
+++ assets/js/timetable/component/ModalProvider.jsx
@@ -44,6 +44,10 @@ type Props = {
   removeTopic(topic: string): void,
 };
 
+function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function formatTime(timeslots, day, startTime, endTime) {
   const timeFormat = 'hh:mm A';
   const dateFormat = 'Do MMMM';
@@ -127,39 +131,60 @@ export default class ModalProvider extends React.Component {
         {speakers.map((speaker) => {
           const hasSocial = 'social' in speaker;
           const icon = 'portrait' in speaker && speaker.portrait ? speaker.portrait : 'https://file.hkoscon.org/speakers/2017/unknown.png';
+          const nickname = hasSocial && 'nickname' in speaker.social && speaker.social.nickname ? <span className="nickname">{ speaker.social.nickname }</span> : '';
           return (
-            <div className="row">
+            <div className="row speaker">
               <div className="col s12 m3 l2">
-                <img src={icon} className="responsive-img circle" alt={speaker.name} />
+                <img src={icon} className="responsive-img circle z-depth-2" alt={speaker.name} />
               </div>
               <div className="col s12 m9 l10">
                 <div className="speaker-name">
                   <span className="name">{speaker.name}</span>
-                  <span className="nickname">{ speaker.social.nickname }</span>
+                  {nickname}
                 </div>
                 <div className="social">
                   {hasSocial && 'github' in speaker.social && (
-                    <a href={`https://github.com/${speaker.social.github}`} rel="noopener noreferrer" target="_blank">
+                    <a
+                      href={`https://github.com/${speaker.social.github}`}
+                      title="Github" alt="Github"
+                      rel="noopener noreferrer" target="_blank"
+                    >
                       <i className="fa fa-github globe" />
                     </a>
                   )}
                   {hasSocial && 'project_github' in speaker.social && (
-                    <a href={speaker.social.project_github} rel="noopener noreferrer" target="_blank">
+                    <a
+                      href={speaker.social.project_github}
+                      title="Github of the Project" alt="Github of the Project"
+                      rel="noopener noreferrer" target="_blank"
+                    >
                       <i className="fa fa-github globe" />
                     </a>
                   )}
                   {hasSocial && 'twitter' in speaker.social && (
-                    <a href={speaker.social.twitter} rel="noopener noreferrer" target="_blank">
+                    <a
+                      href={speaker.social.twitter}
+                      title="Twitter" alt="Twitter"
+                      rel="noopener noreferrer" target="_blank"
+                    >
                       <i className="fa fa-twitter" />
                     </a>
                   )}
                   {hasSocial && 'blog' in speaker.social && (
-                    <a href={speaker.social.blog} rel="noopener noreferrer" target="_blank">
+                    <a
+                      href={speaker.social.blog}
+                      title="Blog" alt="Blog"
+                      rel="noopener noreferrer" target="_blank"
+                    >
                       <i className="fa fa-globe" />
                     </a>
                   )}
                   {hasSocial && 'facebook' in speaker.social && (
-                    <a href={speaker.social.facebook} rel="noopener noreferrer" target="_blank">
+                    <a
+                      href={speaker.social.facebook}
+                      title="Facebook" alt="Facebook"
+                      rel="noopener noreferrer" target="_blank"
+                    >
                       <i className="fa fa-facebook" />
                     </a>
                   )}
@@ -179,7 +204,8 @@ export default class ModalProvider extends React.Component {
     // language
     const lang = this.props.langs[topic.lang] || { name: '' };
     const langSlide = this.props.langs[topic.lang_slide] || null;
-    const langStr = (langSlide !== null) ? `${lang.name} (${langSlide.name})` : lang.name;
+    const langStr = (langSlide !== null && lang.name !== langSlide.name) ?
+      `${lang.name} (${langSlide.name} Slides)` : lang.name;
     return (langStr !== '') ? (
       <div className="language">
         <i className="fa fa-commenting-o" aria-hidden="true" />
@@ -244,7 +270,7 @@ export default class ModalProvider extends React.Component {
     );
     return (
       <div className="sponsor-wrapper">
-        <h2>Sponsored by</h2>
+        <h3>Sponsored by</h3>
         <div className="sponsor row">
           <div className="col m2 logos">
             <p className="logo">
@@ -254,7 +280,7 @@ export default class ModalProvider extends React.Component {
             </p>
           </div>
           <div className="col m10 description">
-            <h3>{sponsor.name}</h3>
+            <h4>{sponsor.name}</h4>
             {sponsor.description.map(paragraph => (<p dangerouslySetInnerHTML={{ __html: paragraph }} />))}
             <div>
               <a
@@ -282,15 +308,17 @@ export default class ModalProvider extends React.Component {
     );
   }
 
-  render() {
+  renderTopic() {
     const topic = this.props.topics.find(t => t.id === this.state.topic) || {};
     const containHtmlTag = topic.description && topic.description[0].indexOf('</') !== -1;
     const description = (!containHtmlTag && topic.description && topic.description[0]) || '';
+    const topicType = capitalize(topic.type || '');
     return (
       <div>
         <div ref={(modal) => { this.modal = modal; }} className="modal modal-fixed-footer">
-          <div className="modal-content">
-            <h4 data-role="title">{topic.title}</h4>
+          <div className="modal-content topic" key={`topic--${topic.id}`}>
+            <span className="topic-type">{topicType}</span>
+            <h2 data-role="title">{topic.title}</h2>
             <div>
               { containHtmlTag ?
                 <div dangerouslySetInnerHTML={{ __html: topic.description[0] }} />
@@ -298,11 +326,13 @@ export default class ModalProvider extends React.Component {
                 description
               }
             </div>
-            {'startTime' in topic && this.renderTime(topic)}
-            {'venue' in topic && this.renderVenue(topic)}
-            {this.renderLang(topic)}
-            {this.renderLevel(topic)}
-            {'requirement' in topic && this.renderReq(topic)}
+            <div className="quick-info">
+              {'startTime' in topic && this.renderTime(topic)}
+              {'venue' in topic && this.renderVenue(topic)}
+              {this.renderLang(topic)}
+              {this.renderLevel(topic)}
+              {'requirement' in topic && this.renderReq(topic)}
+            </div>
             {'speaker' in topic && this.renderSpeakers(topic)}
             {this.renderSponsor(topic)}
             {this.renderSave()}
@@ -312,12 +342,16 @@ export default class ModalProvider extends React.Component {
               Close
             </a>
             <a href={`/2017/topics/${this.state.topic || '#'}`} className="modal-action waves-effect waves-green btn-flat">
-              More
+              Permlink
             </a>
           </div>
         </div>
         {this.props.children}
       </div>
     );
+  }
+
+  render() {
+    return this.renderTopic();
   }
 }
