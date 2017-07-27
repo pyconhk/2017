fb1d6fbf3dafeda760f510a5d8f7b6aec1c09550
Tony Yip <tony@opensource.hk>
2017-07-27 17:15:35 +0800

    Replace fetch with axios

--- assets/js/timetable/fetch/yaml.js
+++ assets/js/timetable/fetch/yaml.js
@@ -1,10 +1,11 @@
+import axios from 'axios';
 import { safeLoad } from 'js-yaml';
 
-/* globals fetch */
-
 export default function loadSpeakers(store, url, action) {
-  return fetch(url)
-    .then(response => response.text())
+  return axios(url, {
+    responseType: 'text',
+  })
+    .then(response => response.data)
     .then(safeLoad)
     .then(data => store.dispatch(action(data)));
 }
