904f9bd7e76bcb5921cf7c38c6d84e28945180d2
Connie <cleung334@gmail.com>
2017-09-15 23:45:43 +0800

    Update CSS to have four cells in one row

--- assets/js/timetable/component/GridCell.jsx
+++ assets/js/timetable/component/GridCell.jsx
@@ -18,7 +18,7 @@ type Props = {
 export default function GridCell(props: Props, context: {openModal: Function}) {
   let lclass = `l${Math.max(Math.floor(10 / props.col), 2)}`;
   if (props.col === 4) lclass = `${lclass} l25`;
-  const classes = classNames(props.className, 's12', `m${props.col > 2 ? 6 : 12}`, lclass, 'col');
+  const classes = classNames(props.className, 's12', `m${props.col > 2 ? 3 : 12}`, lclass, 'col');
   const modal = props.modal;
   if (typeof modal === 'string' && !props.path) {
     /* eslint-disable */
